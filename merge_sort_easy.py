d13714c942ed773b003f2e96ddc320557099cc3a
Rasel Sumon <raselsumon51@gmail.com>
2023-07-22 02:15:20 +0600

    "Images added"

---
+++ merge_sort_easy.py
@@ -0,0 +1,30 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    merged = []
+    i = j = 0
+
+    while i < len(left_half) and j < len(right_half):
+        if left_half[i] < right_half[j]:
+            merged.append(left_half[i])
+            i += 1
+        else:
+            merged.append(right_half[j])
+            j += 1
+
+    merged += left_half[i:]
+    merged += right_half[j:]
+
+    return merged
+
+if __name__ == "__main__":
+    # Example usage:
+    arr = [38, 27, 43, 3, 9, 82, 10]
+    print("Original array:", arr)
+    sorted_arr = merge_sort(arr)
+    print("Sorted array:", sorted_arr)
